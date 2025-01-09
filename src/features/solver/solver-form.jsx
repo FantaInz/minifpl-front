c0d619462aaf7bddaf95a08b602fb9c2dce5308a
syzm <szymone25@gmail.com>
2025-01-09 01:39:18 +0100

    feat: add gameweeks field to form

--- src/features/solver/solver-form.jsx
+++ src/features/solver/solver-form.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box, Stack, Input } from "@chakra-ui/react";
-import { useForm } from "react-hook-form";
+import { Box, Stack, Input, createListCollection } from "@chakra-ui/react";
+import { useForm, Controller } from "react-hook-form";
 
 import { Button } from "@/components/ui/button";
 import { Field } from "@/components/ui/field";
 import { DataListItem, DataListRoot } from "@/components/ui/data-list";
+import {
+  SelectContent,
+  SelectItem,
+  SelectRoot,
+  SelectTrigger,
+  SelectValueText,
+} from "@/components/ui/select";
 
 const SolverForm = ({ freeTransfers, budget, teamId, teamName, onSubmit }) => {
+  const gameweeksCollection = createListCollection({
+    items: [
+      { label: "1", value: "1" },
+      { label: "2", value: "2" },
+      { label: "3", value: "3" },
+      { label: "4", value: "4" },
+      { label: "5", value: "5" },
+    ],
+  });
+
   const items = [
     { label: "ID zespołu", value: teamId },
     { label: "Nazwa zespołu", value: teamName },
@@ -18,6 +35,7 @@ const SolverForm = ({ freeTransfers, budget, teamId, teamName, onSubmit }) => {
   const {
     register,
     handleSubmit,
+    control,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -26,8 +44,13 @@ const SolverForm = ({ freeTransfers, budget, teamId, teamName, onSubmit }) => {
   });
 
   const onFormSubmit = (data) => {
-    onSubmit(data);
-    console.log("Formularz wysłany:", data);
+    const mappedData = {
+      ...data,
+      gameweeks: data.gameweeks.value[0],
+    };
+
+    onSubmit(mappedData);
+    console.log("Formularz wysłany:", mappedData);
   };
 
   return (
@@ -60,8 +83,6 @@ const SolverForm = ({ freeTransfers, budget, teamId, teamName, onSubmit }) => {
           >
             <Input
               placeholder="Wpisz nazwę planu"
-              border="1px solid"
-              borderColor="gray.400"
               width={{ base: "100%", md: "50%" }}
               {...register("planName", {
                 required: "Nazwa planu jest wymagana",
@@ -70,6 +91,39 @@ const SolverForm = ({ freeTransfers, budget, teamId, teamName, onSubmit }) => {
                   message: "Nazwa planu musi mieć co najmniej 3 znaki",
                 },
               })}
+            />
+          </Field>
+
+          <Field
+            label="Liczba kolejek"
+            invalid={!!errors.gameweeks}
+            errorText={errors.gameweeks?.message}
+          >
+            <Controller
+              name="gameweeks"
+              control={control}
+              rules={{
+                required: "Wybierz liczbę kolejek",
+              }}
+              render={({ field }) => (
+                <SelectRoot
+                  name={field.name}
+                  value={field.value}
+                  onValueChange={(value) => field.onChange(value)}
+                  collection={gameweeksCollection}
+                >
+                  <SelectTrigger width={{ base: "100%", md: "20%" }} size="sm">
+                    <SelectValueText placeholder="Wybierz liczbę" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    {gameweeksCollection.items.map((gw) => (
+                      <SelectItem item={gw} key={gw.value}>
+                        {gw.label}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </SelectRoot>
+              )}
             />
           </Field>
 
