f21a453d9e28d66a1a54f38dd231efde5ec8ac90
shamajn <piotr.rowicki02@gmail.com>
2025-01-17 16:36:07 +0100

    removing devtools

--- src/app/provider.jsx
+++ src/app/provider.jsx
@@ -1,5 +1,4 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import * as React from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import PropTypes from "prop-types";
@@ -18,7 +17,6 @@ export const AppProvider = ({ children }) => {
       <ErrorBoundary FallbackComponent={MainErrorFallback}>
         <React.Suspense fallback={<Loading />}>
           <QueryClientProvider client={queryClient}>
-            {import.meta.env.DEV && <ReactQueryDevtools />}
             <AuthLoader renderLoading={() => <Loading />}>
               {children}
             </AuthLoader>
